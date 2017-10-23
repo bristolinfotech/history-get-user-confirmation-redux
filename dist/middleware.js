899fda82741e6315c70cc1a2f1716a395d1d9234
Carl Craig <carl@bristolinfotech.com>
2017-10-23 17:34:10 +0100

    fix redux modules integration issue

--- dist/middleware.js
+++ dist/middleware.js
@@ -3,12 +3,14 @@ var actions_1 = require("./actions");
 exports.createHistoryGetUserConfirmationMiddleware = function (connector, options) {
     if (options === void 0) { options = {}; }
     return function (store) {
+        var modalActive;
         connector.onConfirmation = function (confirmation) {
             store.dispatch({
                 type: actions_1.HISTORY_GET_USER_CONFIRMATION_REQUEST,
                 payload: confirmation.message,
             });
             if (options.modalName) {
+                modalActive = true;
                 store.dispatch({
                     type: '@@reduxModals/OPEN',
                     payload: {
@@ -23,10 +25,14 @@ exports.createHistoryGetUserConfirmationMiddleware = function (connector, option
         return function (next) {
             return function (action) {
                 var result = next(action);
-                if (options.modalName && action.type === '@@reduxModals/CLOSE' && action.payload.name === options.modalName) {
+                if (options.modalName
+                    && action.type === '@@reduxModals/CLOSE'
+                    && action.payload.name === options.modalName
+                    && modalActive) {
                     store.dispatch({
                         type: action.payload.response ? actions_1.HISTORY_GET_USER_CONFIRMATION_ACCEPT : actions_1.HISTORY_GET_USER_CONFIRMATION_REJECT,
                     });
+                    modalActive = false;
                 }
                 else {
                     if (action.type === actions_1.HISTORY_GET_USER_CONFIRMATION_ACCEPT) {
--- src/middleware.tsx
+++ src/middleware.tsx
@@ -15,12 +15,14 @@ export const createHistoryGetUserConfirmationMiddleware = (
   options: HistoryGetUserConfirmationMiddlewareOptions = {},
 ) => {
   return (store: MiddlewareAPI<void>) => {
+    let modalActive: any;
     connector.onConfirmation = (confirmation: HistoryGetUserConfirmation) => {
       store.dispatch({
         type: HISTORY_GET_USER_CONFIRMATION_REQUEST,
         payload: confirmation.message,
       });
       if (options.modalName) {
+        modalActive = true;
         store.dispatch({
           type: '@@reduxModals/OPEN',
           payload: {
@@ -35,10 +37,16 @@ export const createHistoryGetUserConfirmationMiddleware = (
     return (next: Dispatch<void>) => {
       return <A extends AnyAction>(action: A) => {
         const result = next(action);
-        if (options.modalName && action.type === '@@reduxModals/CLOSE' && action.payload.name === options.modalName) {
+        if (
+          options.modalName
+          && action.type === '@@reduxModals/CLOSE'
+          && action.payload.name === options.modalName
+          && modalActive
+        ) {
           store.dispatch({
             type: action.payload.response ? HISTORY_GET_USER_CONFIRMATION_ACCEPT : HISTORY_GET_USER_CONFIRMATION_REJECT,
           });
+          modalActive = false;
         } else {
           if (action.type === HISTORY_GET_USER_CONFIRMATION_ACCEPT) {
             connector.triggerCallbacks(true);
